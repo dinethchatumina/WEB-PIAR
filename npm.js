e97cba7c71bf1aedf486a981ea3a17d0e8038864
dinethchatumina <dinethchatumina@gmail.com>
2025-08-12 08:52:16 +0530

    Create npm.js

---
+++ npm.js
@@ -0,0 +1,11 @@
+Run npm install
+  npm install
+  gulp
+  shell: /usr/bin/bash -e {0}
+npm error code EJSONPARSE
+npm error path /home/runner/work/WEB-PIAR/WEB-PIAR/package.json
+npm error JSON.parse Unexpected end of JSON input while parsing near "...inal\": \"^0.12.0\"\n  }\n"
+npm error JSON.parse Failed to parse JSON data.
+npm error JSON.parse Note: package.json must be actual JSON, not just JavaScript.
+npm error A complete log of this run can be found in: /home/runner/.npm/_logs/2025-08-11T09_38_12_985Z-debug-0.log
+Error: Process completed with exit code 1.
