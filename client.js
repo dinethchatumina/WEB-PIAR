85a24f03b8f443d1d8e856ecc94bb7f1a05ac4de
dinethchatumina <dinethchatumina@gmail.com>
2025-08-13 20:01:08 +0530

    Create  client.js

---
+++ client.js
@@ -0,0 +1,79 @@
+ar client = new ClientJS();
+
+  client.getBrowserData();
+  client.getFingerprint();
+  client.getCustomFingerprint(...);
+
+  client.getUserAgent();
+  client.getUserAgentLowerCase();
+
+  client.getBrowser();
+  client.getBrowserVersion();
+  client.getBrowserMajorVersion();
+  client.isIE();
+  client.isChrome();
+  client.isFirefox();
+  client.isSafari();
+  client.isOpera();
+
+  client.getEngine();
+  client.getEngineVersion();
+
+  client.getOS();
+  client.getOSVersion();
+  client.isWindows();
+  client.isMac();
+  client.isLinux();
+  client.isUbuntu();
+  client.isSolaris();
+
+  client.getDevice();
+  client.getDeviceType();
+  client.getDeviceVendor();
+
+  client.getCPU();
+
+  client.isMobile();
+  client.isMobileMajor();
+  client.isMobileAndroid();
+  client.isMobileOpera();
+  client.isMobileWindows();
+  client.isMobileBlackBerry();
+
+  client.isMobileIOS();
+  client.isIphone();
+  client.isIpad();
+  client.isIpod();
+
+  client.getScreenPrint();
+  client.getColorDepth();
+  client.getCurrentResolution();
+  client.getAvailableResolution();
+  client.getDeviceXDPI();
+  client.getDeviceYDPI();
+
+  client.getPlugins();
+  client.isJava();
+  client.getJavaVersion();
+  client.isFlash();
+  client.getFlashVersion();
+  client.isSilverlight();
+  client.getSilverlightVersion();
+
+  client.getMimeTypes();
+  client.isMimeTypes();
+
+  client.isFont();
+  client.getFonts();
+
+  client.isLocalStorage();
+  client.isSessionStorage();
+  client.isCookie();
+
+  client.getTimeZone();
+
+  client.getLanguage();
+  client.getSystemLanguage();
+
+  client.isCanvas();
+  client.getCanvasPrint();
